07f8ff0711d64f6bdab30853d5a8c905454d5669
Elamaran-Ravichandran <70930612+Elamaran-Ravichandran@users.noreply.github.com>
2021-07-26 17:21:14 +0530

    Update small_files_count.sh

--- small_files_count.sh
+++ small_files_count.sh
@@ -5,15 +5,19 @@
 rm base_path.txt
 rm small_files_count.txt
 rm final_list.csv
-
+rm small_file_list.txt
 # list all folders under given  base path and store it in  base_path.txt
-hdfs dfs -ls -R /user/hive/warehouse/ | awk -F / '/^d/' | awk '{print $8}' >> base_path.txt
+hdfs dfs -ls  /user/hive/warehouse/*/ | awk -F / '/^d/' | awk '{print $8}' >> base_path.txt
 
 # Loop every line in base_path.txt and filter small files  recursively (with respect to given threshold size -64MB)
 
 while IFS="" read -r line || [ -n "$line" ]
 do
-        count=$(hdfs dfs -ls -R ${line} | awk '{if($5<67108864) print $8 && $1 !~ "^d"}' | wc -l)
+
+        file_name=$(hdfs dfs -ls -R ${line} | awk '{if($5<3145728) print $8}')
+        echo "$file_name" >> small_file_list.txt
+#write the small file names in small_file_list.txt
+        count=$(hdfs dfs -ls -R ${line} | awk '{if($5<3145728) print $8 && $1 !~ "^d"}' | wc -l)
         echo "$line","$count"  >> small_files_count.txt
 # write file path and count of files under every folder in out
 done < base_path.txt
